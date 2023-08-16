c089e34d62f2cb58de24bc1a811e34a91e5e0a72
Ruth Bamgbose <rulibams16@gmail.com>
2023-08-16 03:57:38 -0700

    ko

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,11 @@
 int main(void)
 {
 	int n;
-	for (n = 0; n < 16; n++)
-	{
-		if (n < 10)
-			putchar(n + '0');
-		else
-			putchar(n - 10 + 'a');
-		putchar('\n');
-	}
+	for (n = 0; n < 9; n++)
+		putchar(n);
+
+	for (n = 'a'; n <= 'f'; n++)
+		putchar(n);
+	putchar('\n');
 	return (0);
 }
