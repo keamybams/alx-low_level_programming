1a047f6b6b2766d0680c02e09925dc49fb536d18
Ruth Bamgbose <rulibams16@gmail.com>
2023-08-16 02:32:20 -0700

    lop

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
