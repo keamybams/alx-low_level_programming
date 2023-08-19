94e765eacb6cead139e49c9c18b17e5239e55790
Ruth Bamgbose <rulibams16@gmail.com>
2023-08-18 22:23:37 -0700

    po

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, n, m;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (n = i + 1; n <= '9'; n++)
+		{
+			for (m = n + 1; m <= '9'; m++)
+			{
+				putchar(i);
+				putchar(n);
+				putchar(m);
+
+				if(i != '7' && n != '8' && m != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
