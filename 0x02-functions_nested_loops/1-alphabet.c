66e95430f97487ae1109014199e599d2ae36ed55
Ruth Bamgbose <rulibams16@gmail.com>
2023-08-22 14:12:32 -0700

    lo

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,18 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_alphabet - prints the letters lowercase
- * Return: Nothing
+ * _putchar - Custom implementation of putchar.
+ * @c: The character to print.
+ * Return: 1 on success, -1 on error.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
  */
 void print_alphabet(void)
 {
@@ -11,4 +21,10 @@ void print_alphabet(void)
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
+}
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
 }
