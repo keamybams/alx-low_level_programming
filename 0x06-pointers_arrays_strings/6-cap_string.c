116f61d00996ee2875a635d3803794daf6d1fbcf
Ruth Bamgbose <rulibams16@gmail.com>
2023-09-02 13:17:22 -0700

    correct

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,42 @@
-#include "main.h"
-#include <ctype.h>
+int check_seperators(char c);
 
 /**
- * cap_string - Capitalizes all words in a string.
- * @s: The input string.
- * Return: A pointer to the modiied string.
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to capitalize letters
+ * Return: pointer to s
  */
 char *cap_string(char *s)
 {
-	int new_word = 1;
 	int i = 0;
 
-	for (int i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
-		{
-			new_word = 1;
-		}
-		else if (new_word && isalpha(s[i]) && islower(s[i]))
-		{
-			s[i] = toupper(s[i]);
-			new_word = 0;
-		}
-		else
-		{
-			new_word = 0;
-		}
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
 	}
 
 	return (s);
+}
+
+/**
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
 }
