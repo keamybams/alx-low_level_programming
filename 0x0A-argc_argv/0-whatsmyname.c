42dc481dc1ccba932b9e01b6fd76efdc390c52e2
ruth bamgbose <rulibams16@gmail.com>
2023-10-10 13:51:57 -0700

    pol

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,14 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	char *name = argv[0];
-	char *last_slash = strrchr(name, '/');
+	(void) argc;
 
-	if (last_slash != NULL)
-	{
-		name = last_slash + 1;
-	}
-
-	printf("%s\n", name);
+	printf("%s\n", argv[0]);
 	return (0);
 }
