b8ee0c3beec1c60dde525b4e7ae5824f7a938023
ruth bamgbose <rulibams16@gmail.com>
2023-10-10 13:16:16 -0700

    poll

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ * */
+
+int main(int argc, char *argv[])
+{
+	char *name = argv[0];
+	char *last_slash = strrchr(name, '/');
+
+	if (last_slash != NULL)
+	{
+		name = last_slash + 1;
+	}
+	printf("%\n", name);
+	return (0);
+}
