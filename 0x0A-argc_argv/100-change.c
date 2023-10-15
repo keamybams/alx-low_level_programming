5c22f93ea8022e8d684cba5846f62a1e416e1b0b
ruth bamgbose <rulibams16@gmail.com>
2023-10-15 04:59:34 -0700

    lo

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * minCoins - returns the minimum number of coins for a given value
- * @value: the value to be changed
+ * minCoins - Calculate the minimum number of coins needed for a given value.
+ * @value: The value for which to calculate the minimum number of coins.
  *
- * Description: This function uses a recursive approach to find the minimum
+ * This function uses a recursive approach to find the minimum
  * number of coins for a given value. It is based on the following formula:
  *
  * minCoins(V) = {
@@ -11,83 +11,75 @@
  *   min(i) {1 + minCoins(V - C_i)} if V > 0 and C_i <= V
  * }
  *
- * where V is the value, C_i is the i-th coin value, and min(i) means taking
+ * Where V is the value, C_i is the i-th coin value, and min(i) means taking
  * the minimum over all possible i.
  *
- * Return: the minimum number of coins for the value
+ * Return: The minimum number of coins required for the value.
  */
+int minCoins(int value);
+
 int minCoins(int value)
 {
-	/* If the value is zero, no coins are needed */
-	int result;
+    int result;
+    int coins[] = {25, 10, 5, 2, 1};
+	int n, i;
 
-	if (value == 0)
-	{
-		return (0);
-	}
+    if (value == 0)
+    {
+        return 0;
+    }
 
-	result = INT_MAX;
+    result = INT_MAX;
 
-	/* Define the coin values in an array */
-	int coins[] = {25, 10, 5, 2, 1};
+    n = sizeof(coins) / sizeof(int);
 
-	/* Define the number of coins available */
-	int n = sizeof(coins) / sizeof(int);
 
-	/* Loop through all the coins */
-	for (int i = 0; i < n; i++)
-	{
-		/* If the coin value is less than or equal to the value */
-		if (coins[i] <= value)
-		{
-			/* Recursively find the minimum number of coins for the remaining value */
-			int subResult = minCoins(value - coins[i]);
+    for (i = 0; i < n; i++)
+    {
+        if (coins[i] <= value)
+        {
+            int subResult = minCoins(value - coins[i]);
 
-			/* If the subResult is valid and smaller than the current result */
-			if (subResult != INT_MAX && subResult + 1 < result)
-			{
-				/* Update the result with the subResult plus one coin */
-				result = subResult + 1;
-			}
-		}
-	}
+            if (subResult != INT_MAX && subResult + 1 < result)
+            {
+                result = subResult + 1;
+            }
+        }
+    }
 
-	/* Return the result */
-	return (result);
+    return result;
 }
 
 /**
- * main - takes the input and prints the output
- * @argc: the number of arguments
- * @argv: the array of arguments
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success, 1 on error.
  */
+
 int main(int argc, char *argv[])
 {
-	/* If the number of arguments is not exactly one, print an error and return 1 */
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+    int value;
+    int result;
 
-	/* Parse the argument as an integer using atoi function */
-	int value = atoi(argv[1]);
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-	/* If the value is negative, print zero and return */
-	if (value < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+    value = atoi(argv[1]);
 
-	/* Find the minimum number of coins for the value using minCoins function */
-	int result = minCoins(value);
+    if (value < 0)
+    {
+        printf("0\n");
+        return 0;
+    }
 
-	/* Print the result */
-	printf("%d\n", result);
+    result = minCoins(value);
 
-	/* Return zero to indicate success */
-	return (0);
+    printf("%d\n", result);
+
+    return 0;
 }
